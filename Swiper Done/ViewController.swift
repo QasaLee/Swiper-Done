991df4758aad9582d3d62337c061329f5b9d4a31
Nate <intoqasa@gmail.com>
2018-06-21 22:54:25 +0800

    Initial Commit

---
+++ Swiper Done/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swiper Done
+//
+//  Created by M78 on 6/21/18.
+//  Copyright © 2018 M78. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
